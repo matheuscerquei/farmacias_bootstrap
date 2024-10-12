277f770b9a62da0a8975914eab1b5ca71bd6fb4e
AugustoMizu <augusto10barros@gmail.com>
2024-10-11 21:02:29 -0300

    terminar

---
+++ CRUDlogin.php
@@ -0,0 +1,8 @@
+<?php 
+require "configPDO.php";
+
+function cadastrar ($email, $senha, $tipo){
+
+    
+}
+?>
---
+++ configPDO.php
@@ -0,0 +1,3 @@
+<?php 
+$sql = new PDO("mysql:dbname=BdVidaSaudavel;host=localhost:3306","root","cimatec");
+?>
