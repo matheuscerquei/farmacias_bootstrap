de4580c636ac58be57bede1bc2e3270fc134be4a
AugustoMizu <augusto10barros@gmail.com>
2024-10-15 21:23:30 -0300

    terminar as funcões de estoque e venda

--- CRUDestoque.php
+++ CRUDestoque.php
@@ -27,17 +27,21 @@ class produtos
         $c = new config();
         $pdo = $c->getPDO();
 
-        $sql = $pdo->query("SELECT * FROM produtos WHERE nome = :nome AND categoria = :categoria");
+        $sql = $pdo->query("SELECT * FROM produtos WHERE nome = :nome AND categoria = :categoria;");
         $sql->bindValue(':nome', $p->nome);
         $sql->bindValue(':categoria', $p->categoria);
 
-        $sql->execute();
+        if (!$sql->execute()) {
+            // caso haja uma falha na conexão
+            echo '<script>console.log("falha na conexão com o banco")</script>';
+            return false;
+        }
         if ($sql->rowCount() > 0) {
             // produto com nome e categoria já cadastradas
             return false;
         } else {
             //cadastra o produto novo 
-            $sql = $pdo->prepare("INSERT INTO produtos (nome, preco, quantidade, categoria, data_validade) VALUES (:nome, :preco, :quantidade, :categoria, :data_validade)");
+            $sql = $pdo->prepare("INSERT INTO produtos (nome, preco, quantidade, categoria, data_validade) VALUES (:nome, :preco, :quantidade, :categoria, :data_validade);");
             $sql->bindValue(':nome', $p->nome);
             $sql->bindValue(':preco', $p->preco);
             $sql->bindValue(':quantidade', $p->quantidade);
@@ -49,10 +53,54 @@ class produtos
                 return true;
             } else {
                 //cadastro falhou
+                echo '<script>console.log("falha na conexão com o banco")</script>';
                 return false;
             }
         }
     }
 
-    function 
+    function retornaTodosProdutos() //retorna os protudos cadstrados como uma lista ordenada
+    {
+        $c = new config();
+        $pdo = $c->getPDO();
+
+        $sql = $pdo->query("SELECT * FROM produtos");
+        if (!$sql->execute()) {
+            echo '<script>console.log("falha na conexão com o banco")</script>';
+            return false;
+        }
+
+        if ($sql->rowCount() > 0) {
+            $lista = [];
+            $lista = $sql->fetchAll(PDO::FETCH_ASSOC);
+            return $lista;
+        } else {
+            // houve algum erro na conexão com o banco
+            echo '<script>console.log("")</script>';
+            return false;
+        }
+    }
+
+    function retornaPorNome(produtos $p) // retorna todos regsitros encontrados com um mesmo nome
+    {
+        $c = new config();
+        $pdo = $c->getPDO();
+
+        $sql = $pdo->prepare("SELECT * FROM produtos WHERE nome = :nome");
+        $sql->bindValue(':nome', $p->nome);
+
+        if (!$sql->execute()) {
+            echo '<script>console.log("falha na conexão com o banco")</script>';
+            return false;
+        }
+        if ($sql->rowCount() > 0) {
+            // retorna os registros encontrados
+            $lista = [];
+            $lista = $sql->fetchAll();
+            return $lista;
+        }else{
+            // nome não encontrado no banco
+            return false;
+        }
+    }
 }
